739b78743df7691ba46068d2fc0e85e750b2d190
Kaustubh1206 <kaustubhgidh06@gmail.com>
2025-08-12 17:27:38 +0530

    Update exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys # any exception which is system is controlling - sys library will have the information about the exceptiomn
-import logging
+from src.logger import logging 
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # this will give you info about where is exception happened , which line
@@ -17,12 +17,3 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-    
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero ")
-        raise CustomException(e,sys)
-    
-    

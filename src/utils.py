7c2f90e8829eed47c3fc7d8a65575ed4f95cc1dd
Kautubh1206 <kaustubhgidh06@gmail.com>
2025-09-20 19:21:00 -0400

    Predicted pipeline  and app.py

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ from src.logger import logging
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-
+import pickle
 import dill # helps to create pickle file
 
 # Code to save object in form 
@@ -54,5 +54,13 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]]= test_model_score
 
             return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
